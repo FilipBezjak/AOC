2ec00e67c16d349d8c0cf2bf0c98de20dd0910c7
Filip Bezjak <Filip.bezjak@gmail.com>
2020-12-05 18:22:25 +0100

    day 5

--- tester.ml
+++ tester.ml
@@ -1,10 +1,76 @@
-let rec zadnja_funkcija (str)=
-  let rec zadnja_aux acc stringg =
-    match stringg with
-    |x::xs ->
-    let (a::_) = String.split_on_char ':' x in
-    if not (a <> "cid") then zadnja_aux (acc+1) xs
-    else zadnja_aux acc xs
-    |_-> acc
+let explode str =
+  let rec exp a b =
+    if a < 0 then b
+    else exp (a - 1) (str.[a] :: b)
   in
-  (zadnja_aux 0 str )= 7
+  exp (String.length str - 1) []
+
+let  izracuna list=
+    let rec izracuna_aux min max list l d=
+      let index1 = (1 + max - min)/2 + min in
+      let index2 = (1 + d - l)/2 + l in
+      match list with
+      | 'F'::rest -> izracuna_aux min (index1-1) rest l d
+      | 'B'::rest -> izracuna_aux index1 max rest l d
+      | 'L'::rest -> izracuna_aux index1 max rest l (index2-1)
+      | 'R'::rest -> izracuna_aux index1 max rest index2 d
+      | [] -> (min,l)
+      |_ -> failwith "napacen vnos"
+      in
+    izracuna_aux 0 127 list 0 7 
+(* funckija obrne seznam, kopirana iz stack overflow*)
+let reverse l =
+  let rec rev_acc acc list =
+    match list with
+    | hd::tl -> rev_acc (hd::acc) tl
+    | [] -> acc
+  in
+  rev_acc [] l
+
+
+let id (vrsta, sedez)=
+  vrsta * 8 + sedez
+
+  (*kopirano z stack overflow. Sortira seznam po velikosti*)
+  let rec sort lst =
+    match lst with
+      [] -> []
+    | head :: tail -> insert head (sort tail)
+  and insert elt lst =
+    match lst with
+    |  [] -> [elt]
+    | head :: tail -> if elt <= head then elt :: lst else head :: insert elt tail;;
+
+let sez prvi zadnji =
+  let rec aux acc prvi zadnji  =
+    if prvi < zadnji then aux (prvi::acc) (prvi + 1) zadnji
+    else (zadnji::acc) in
+    aux [] prvi zadnji
+
+
+    let od_min_do_max list=
+      let list_rev =  reverse list in
+      match list with
+      |prvi::xs->
+      let prvi= prvi in
+      match list_rev with
+      |zadnji::x->
+      let zadnji = zadnji in
+      sez prvi zadnji
+
+    let rec poisce_manjkajoce prvizadnji list=
+        match prvizadnji with
+        |x::xs->
+        if List.mem x list then poisce_manjkajoce xs list else x
+
+    let naloga1 (data: string)=
+      let lines = String.split_on_char '\n' data in
+      let sortiran = lines |>List.map explode|> List.map izracuna |> List.map id
+      |>sort in
+      let sezz = sortiran |> od_min_do_max in
+      poisce_manjkajoce sezz sortiran
+      
+
+
+
+
--- windows.ml
+++ windows.ml
@@ -440,6 +440,88 @@ module Solver4 : Solver = struct
   
   
 end
+module Solver5 : Solver = struct
+
+let explode str =
+  let rec exp a b =
+    if a < 0 then b
+    else exp (a - 1) (str.[a] :: b)
+  in
+  exp (String.length str - 1) []
+
+let  izracuna list=
+    let rec izracuna_aux min max list l d=
+      let index1 = (1 + max - min)/2 + min in
+      let index2 = (1 + d - l)/2 + l in
+      match list with
+      | 'F'::rest -> izracuna_aux min (index1-1) rest l d
+      | 'B'::rest -> izracuna_aux index1 max rest l d
+      | 'L'::rest -> izracuna_aux index1 max rest l (index2-1)
+      | 'R'::rest -> izracuna_aux index1 max rest index2 d
+      | [] -> (min,l)
+      |_ -> failwith "napacen vnos"
+      in
+    izracuna_aux 0 127 list 0 7 
+
+let id (vrsta, sedez)=
+  vrsta * 8 + sedez
+
+  (* funckija obrne seznam, kopirana iz stack overflow*)
+let reverse l =
+  let rec rev_acc acc list =
+    match list with
+    | hd::tl -> rev_acc (hd::acc) tl
+    | [] -> acc
+  in
+  rev_acc [] l
+
+
+  (*kopirano z stack overflow. Sortira seznam po velikosti*)
+  let rec sort lst =
+    match lst with
+      [] -> []
+    | head :: tail -> insert head (sort tail)
+  and insert elt lst =
+    match lst with
+    |  [] -> [elt]
+    | head :: tail -> if elt <= head then elt :: lst else head :: insert elt tail;;
+
+let sez prvi zadnji =
+  let rec aux acc prvi zadnji  =
+    if prvi < zadnji then aux (prvi::acc) (prvi + 1) zadnji
+    else (zadnji::acc) in
+    aux [] prvi zadnji
+
+
+    let od_min_do_max list=
+      let list_rev =  reverse list in
+      match list with
+      |prvi::xs->
+      let prvi= prvi in
+      match list_rev with
+      |zadnji::x->
+      let zadnji = zadnji in
+      sez prvi zadnji
+
+    let rec poisce_manjkajoce prvizadnji list=
+        match prvizadnji with
+        |x::xs->
+        if List.mem x list then poisce_manjkajoce xs list else x
+
+    let naloga2 (data: string) prva =
+      let lines = String.split_on_char '\n' data in
+      let sortiran = lines |>List.map explode|> List.map izracuna |> List.map id
+      |>sort in
+      let sezz = sortiran |> od_min_do_max in
+      poisce_manjkajoce sezz sortiran |> string_of_int
+
+    let naloga1 (data: string)=
+      let lines = String.split_on_char '\n' data in
+      lines |>List.map explode|> List.map izracuna |> List.map id
+      |>List.fold_left max 0 |> string_of_int
+
+
+end
 
 let choose_solver : string -> (module Solver) = function
   | "0" -> (module Solver4)
@@ -447,10 +529,11 @@ let choose_solver : string -> (module Solver) = function
   | "2" -> (module Solver4)
   | "3" -> (module Solver4)
   | "4" -> (module Solver4)
+  | "5" -> (module Solver5)
   | _ -> failwith "ni se reseno"
 
 let main () =
-  let day = "4" in (*Sys.argv.(1) in*)
+  let day = "5" in (*Sys.argv.(1) in*)
   print_endline ("Solving DAY: " ^ day);
   let (module Solver) = choose_solver day in
   let input_data = preberi_datoteko ("data/day_" ^ day ^ ".in") in
